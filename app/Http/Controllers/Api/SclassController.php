88451c1bbb4f4684d879d07b739125566069346e
Ankit <ankitdm69@gmail.com>
2021-10-24 11:41:51 +0530

    first commit

---
+++ app/Http/Controllers/Api/SclassController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use App\Models\Sclass;
+
+class SclassController extends Controller
+{
+    //
+    public function Index()
+    {
+        $sclass = Sclass::latest()->get();
+        return response()->json($sclass);
+    }
+
+    public function Store(Request $request)
+    {
+        $validateData = $request->validate([
+            'class_name' => 'required|unique:sclasses|max:25'
+        ]);
+
+        Sclass::insert([
+            'class_name' => $request -> class_name,
+        ]);
+        return response('Student Class Inserted Succesfully');
+    }
+
+    public function Edit($id){
+        $sclass = Sclass::findOrFail($id); 
+        return response()->json($sclass);       
+    }
+
+    public function Update($id) {
+        
+    }
+}
